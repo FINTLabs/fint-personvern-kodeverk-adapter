ccae6de3ef59adbde5c0a6654bd208a362443e54
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-08-09 08:55:52 +0200

    format

--- src/main/java/no/fintlabs/model/personopplysning/PersonopplysningPublisher.java
+++ src/main/java/no/fintlabs/model/personopplysning/PersonopplysningPublisher.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 
-
 @Slf4j
 @Service
 public class PersonopplysningPublisher extends ResourcePublisher<PersonopplysningResource, ResourceRepository<PersonopplysningResource>> {
