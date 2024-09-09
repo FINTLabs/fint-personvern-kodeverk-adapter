784bc430375ce9eb5f92e8a9b9a4f69dc1fb9f6b
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-09-09 13:51:58 +0200

    add validator on personopplysningresource

--- src/main/java/no/fintlabs/model/personopplysning/PersonopplysningResourceValidator.java
+++ src/main/java/no/fintlabs/model/personopplysning/PersonopplysningResourceValidator.java
@@ -1,13 +1,14 @@
 package no.fintlabs.model.personopplysning;
 
 import no.fint.model.resource.personvern.kodeverk.PersonopplysningResource;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class PersonopplysningResourceValidator {
 
     public boolean isResourceValid(PersonopplysningResource personopplysningResource) {
-        if (personopplysningResource.getKode() != null) return false;
-        return true;
+        return personopplysningResource.getKode() != null &&
+                personopplysningResource.getNavn() != null &&
+                personopplysningResource.getSystemId() != null;
     }
 }
