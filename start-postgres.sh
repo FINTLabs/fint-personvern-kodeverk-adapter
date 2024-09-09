8c1d982a30866eda398771195381715f473b2939
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-09-09 11:00:13 +0200

    restore database-lagring for documentation purposes

--- start-postgres.sh
+++ start-postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name fint-personvern-kodeverk-adapter-postgres -p 5436:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_DB=fint-personvern-kodeverk-adapter -d postgres
+docker run --rm --name fint-personvern-kodeverk-adapter -p 5436:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_DB=fint-personvern-kodeverk-adapter -d postgres
