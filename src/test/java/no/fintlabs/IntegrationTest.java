423d7f5209d1200a390c67a6cac123824c843f7c
Joakim Flatby <joakim.flatby@novari.no>
2025-11-14 07:57:55 +0100

    tests: add integration test contextLoads with testcontainers

---
+++ src/test/java/no/fintlabs/IntegrationTest.java
@@ -0,0 +1,37 @@
+package no.fintlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles({"local", "db"})
+@SpringBootTest
+@Testcontainers
+public class IntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("fint.database.username", postgres::getUsername);
+        registry.add("fint.database.password", postgres::getPassword);
+
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
