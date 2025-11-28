e26c1153bd3986543178241a070cdfce141037ce
Joakim Flatby <joakim.flatby@novari.no>
2025-11-28 12:36:01 +0100

    test: Use Postgres 18 in integration test

--- src/test/java/no/fintlabs/IntegrationTest.java
+++ src/test/java/no/fintlabs/IntegrationTest.java
@@ -15,7 +15,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class IntegrationTest {
 
     @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18")
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test");
@@ -30,6 +30,19 @@ public class IntegrationTest {
 
     }
 
+    /**
+     * This simple test gives some coverage by verifying that the application and the beans can start up correctly.
+     * However, no assertions are made to verify that the data is actually processed correctly, so ideally we should
+     * have more specific tests for that.
+     * This has proven difficult to implement due to the asynchronous nature of the data processing which makes the
+     * tests fail when trying to use @SpyBean and ArgumentCaptor to verify interactions with the publishers.
+     * -
+     * When using @SpyBean on a Publisher(like we do in utdanning-larling-adapter), the test fails with
+     * 'Cannot invoke "java.util.concurrent.locks.ReentrantLock.lock()" because "lock" is null'.
+     * When following the flow with the debugger, it seems that the constructor of BehandlingPublisher is called
+     * on a different instance than the one subscribe() is called on, causing the lock to be null when subscribe() is
+     * called.
+     */
     @Test
     public void contextLoads() {
     }
